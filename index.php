e18b1c67c2f39d285ba15e096fbcadcdcd6ff8be
karthi-2 <69393002+karthi-2@users.noreply.github.com>
2021-08-26 23:08:03 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,30 @@
 <?php 
-$to_email="sec19cs080@sairamtap.edu.in";
+require 'class/class.phpmailer.php';
+$mail=new PHPMALIER;
+$mail->IsSMTP();
+$mail->Host='smtpout.secureserver.net';
+$mail->Port='80';
+$mail->SMTPAuth=true;
+$mail->Username='karthi@ info';
+$mail->Password='password';
+$mail->SMTPSecure='';
+$mail->From='karthi@info';
+$mail->FromName='karthi';
+$mail->AddAddress('karthi');
+$mail->IsHTML(true);
+$mail->Subject='password';
+$message_body='otp';
+$mail->Body=$message_body;
+if($mail->send())
+{
+	echo "Success";
+}
+else{
+	echo "fail";
+}
+
+
+/*$to_email="sec19cs080@sairamtap.edu.in";
 $subject="To learn";
 $body="Hi, This is sample email";
 $headers="From: deepdrones@gmail.com";
@@ -18,4 +43,5 @@ echo "$karthi";
 </html>
 <?php
 echo  "$k1";
+*/
 ?>
