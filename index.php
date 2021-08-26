d8fcb03d1335b0b3143c5df431367015def31f6b
karthi-2 <69393002+karthi-2@users.noreply.github.com>
2021-08-26 19:38:46 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php 
+$to_email="sec19cs080@sairamtap.edu.in";
+$subject="To learn";
+$body="Hi, This is sample email";
+$headers="From: deepdrones@gmail.com";
+if(mail($to_email,$subject,$body,$headers)){
+	echo "Email successfully sent to $to_email...";
+}
+else{
+	echo "Failed";
+}
+?>
